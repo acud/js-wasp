3c1c3ea7eaefb310d41a12915c63c636e4cc8435
acud <12988138+acud@users.noreply.github.com>
2020-11-09 10:46:56 +0100

    Two workers (#5)
    * three messages pass but signature is invalid
    
    clean

--- src/dialer.js
+++ src/dialer.js
@@ -1,11 +1,6 @@
 'use strict'
 /* eslint-disable no-console */
 
-/*
- * Dialer Node
- */
-
-
 const multiaddr = require('multiaddr')
 const PeerId = require('peer-id')
 const Node = require('./libp2p/libp2p-bundle')
@@ -25,18 +20,53 @@ console.log(ww.getPrivateKey())
 
 
 let genSignData = function (under, over) {
-  //func generateSignData(underlay, overlay []byte, networkID uint64) []byte {
-  //networkIDBytes := make([]byte, 8)
-  //binary.BigEndian.PutUint64(networkIDBytes, networkID)
-  //signData := append(underlay, overlay...)
-  //return append(signData, networkIDBytes...)
-  //}
   let networkId = Uint8Array.from([0, 0, 0, 0, 0, 0, 0, 1])
   let u1 = Uint8Array.from(under)
   let o1 = Uint8Array.from(over)
-  let data = Buffer.concat([u1, o1, networkId])// [].push(...u1) //[].concat(u1,o1,networkId)
-  console.log(data)
-  return data
+ 
+  let data = Buffer.concat([u1, o1, networkId])
+  let eth =Uint8Array.from(Array.from("\x19Ethereum Signed Message:\n" + data.length))
+  let data1 = Buffer.concat([eth,data])
+  console.log(data1)
+  return data1
+}
+
+function passthroughToStream(ps, stream) {
+  const asyncIterable = {
+    async*[Symbol.asyncIterator]() {
+      for await (const chunk of ps) {
+        yield chunk;
+      }
+    }
+  };
+
+  pipe(
+    // Read from passthrough stream (the source)
+    asyncIterable,
+    // Encode with length prefix (so receiving side knows how much data is coming)
+    lp.encode(),
+    // Write to the stream (the sink)
+    stream.sink
+  )
+}
+
+function streamToPs(stream, ps) {
+  pipe(
+    // Read from the stream (the source)
+    stream.source,
+    // Decode length-prefixed data
+    lp.decode(),
+    // Sink function
+    async function (source) {
+      // For each chunk of data
+      for await (const msg of source) {
+        // Output the data as a utf8 string
+        let v = new Uint8Array(msg.slice());
+        ps.write(v)
+      }
+    }
+
+  )
 }
 
 
@@ -104,7 +134,7 @@ async function run() {
   })
 
   // Add peer to Dial (the listener) into the PeerStore
-  const listenerMultiaddr = '/ip4/127.0.0.1/tcp/7070/p2p/16Uiu2HAmTjKQVuuZRtrAKWnMf2p1FYAUqmfHh9DdFicyCvZCRffN'
+  const listenerMultiaddr = "/ip4/127.0.0.1/tcp/7070/p2p/16Uiu2HAmGZYxnQG2Mhibjxyjfn3TuYE7PeFFxuAoAac9fZoy8viA"//'/ip4/127.0.0.1/tcp/7070/p2p/16Uiu2HAmTjKQVuuZRtrAKWnMf2p1FYAUqmfHh9DdFicyCvZCRffN'
 
   // Start the dialer libp2p node
   await dialerNode.start()
